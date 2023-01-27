9ab6c5637dac84a9ceb4db3dacff84a4afa28a65
ElidaSeptriani <113493736+ElidaSeptriani@users.noreply.github.com>
2023-01-27 20:40:55 +0700

    Add files via upload

---
+++ UTS/gaji-service/src/main/java/com/eli/gaji/service/VO/ResponseTemplateVO.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.eli.gaji.service.VO;
+
+import com.eli.gaji.service.entity.Gaji;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author ASUS
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseTemplateVO {
+    private Gaji gaji;
+    private Pegawai pegawai;
+}
